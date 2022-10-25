26468c3e1d7b80c6ba92bb80f7a4a0f523c72aee
styven <styvenn16@gmail.com>
2022-10-25 15:26:27 -0300

    Primeiro

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,15 @@
+import './style.css';
+
+export function CardGame(){
+ return  `
+
+ <article class = "card-game">
+ 
+  <img src="image/GrupoLobo.png" alt="Logo tribal" />
+ 
+  </article>
+ 
+
+ `
+
+}
